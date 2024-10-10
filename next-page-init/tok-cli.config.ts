4a76c55953ee493840bf8c400f3d5c6e4652a574
dave17code <dave17code@gmail.com>
2024-10-10 23:18:28 +0900

    241010Home

---
+++ next-page-init/tok-cli.config.ts
@@ -0,0 +1,59 @@
+import { RootConfig } from '@toktokhan-dev/cli'
+import { commit } from '@toktokhan-dev/cli-plugin-commit'
+import { genApi } from '@toktokhan-dev/cli-plugin-gen-api-react-query'
+import { genIcon } from '@toktokhan-dev/cli-plugin-gen-icon-chakra'
+import { genImg } from '@toktokhan-dev/cli-plugin-gen-img'
+import { genRoutePage } from '@toktokhan-dev/cli-plugin-gen-route-pages'
+import { genTheme } from '@toktokhan-dev/cli-plugin-gen-theme-chakra'
+
+// 새로 만든 플러그인 import
+import { printText } from './src/scripts/plugin-text'
+
+// scripts 폴더 경로 유지
+
+const config: RootConfig<{
+  plugins: [
+    typeof genImg,
+    typeof genRoutePage,
+    typeof genApi,
+    typeof genTheme,
+    typeof genIcon,
+    typeof commit,
+    typeof printText,
+  ]
+}> = {
+  plugins: [genImg, genRoutePage, genApi, genTheme, genIcon, commit, printText],
+  'gen:img': {
+    input: 'public/images',
+    oneDepth: true,
+    basePath: '/images',
+  },
+  'gen:route': {
+    oneDepth: true,
+  },
+  'gen:api': {
+    swaggerSchemaUrl: 'https://sales-api-dev.pluuug.com/openapi.json/',
+  },
+  'gen:theme': {
+    tokenModes: {
+      colors: {
+        light: 'light',
+        dark: 'dark',
+      },
+      textStyles: {
+        base: 'mobile',
+        sm: 'tablet',
+        md: 'desktop',
+      },
+    },
+  },
+  'gen:icon': {
+    input: 'public/icons',
+  },
+  // printText 플러그인의 설정 추가
+  'print:text': {
+    output: 'generated/print-text.txt', // 기본 출력 경로
+    input: 'Hello World!', // input 설정 추가
+  },
+}
+export default config
