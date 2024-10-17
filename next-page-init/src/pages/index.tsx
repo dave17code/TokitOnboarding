21a5e643f25895fcf43d3c5732cf3c2290032d6a
dave17code <dave17code@gmail.com>
2024-10-18 07:57:20 +0900

    T-21

--- next-page-init/src/pages/index.tsx
+++ next-page-init/src/pages/index.tsx
@@ -10,8 +10,9 @@ function HomePage() {
     <>
       {/* output: 똑똑한개발자 | 메인 */}
       {/* titleTemplate는 /configs/seo/config.ts에서 변경 가능합니다. */}
-      <NextSeo title="메인" />
-      <HomeLayout content={<Home />} />
+      {/* <NextSeo title="메인" />
+      <HomeLayout content={<Home />} /> */}
+      <h1>Hello World</h1>
     </>
   )
 }
